17de3dce76a7342e801a0b0dc9b7efbdb366fc52
Edgar Adalid Cisneros Villa <edgar.cisneros@tautenet.com.mx>
2024-06-05 15:28:51 -0600

    Prettier

--- JS/script.js
+++ JS/script.js
@@ -1,67 +1,88 @@
-document.getElementById('formulario').addEventListener('submit', function(event) {
+document
+  .getElementById("formulario")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
-    var nombre = document.getElementById('nombre').value;
-    var mensaje = '¡Hola, ' + nombre + '! Gracias por visitar mi página web.';
-    document.getElementById('mensaje').textContent = mensaje;
-});
+    var nombre = document.getElementById("nombre").value;
+    var mensaje = "¡Hola, " + nombre + "! Gracias por visitar mi página web.";
+    document.getElementById("mensaje").textContent = mensaje;
+  });
 
 function calcular() {
-    var num1 = parseFloat(document.getElementById('numero1').value);
-    var num2 = parseFloat(document.getElementById('numero2').value);
-    var suma = num1 + num2;
-    var resta = num1 - num2;
-    var multiplicacion = num1 * num2;
-    var division = num1 / num2;
-    var resultado = 'Suma: ' + suma + ', Resta: ' + resta + ', Multiplicación: ' + multiplicacion + ', División: ' + division;
-    document.getElementById('resultado').textContent = resultado;
+  var num1 = parseFloat(document.getElementById("numero1").value);
+  var num2 = parseFloat(document.getElementById("numero2").value);
+  var suma = num1 + num2;
+  var resta = num1 - num2;
+  var multiplicacion = num1 * num2;
+  var division = num1 / num2;
+  var resultado =
+    "Suma: " +
+    suma +
+    ", Resta: " +
+    resta +
+    ", Multiplicación: " +
+    multiplicacion +
+    ", División: " +
+    division;
+  document.getElementById("resultado").textContent = resultado;
 }
 
 function calcular_rectagulo() {
-    var altura = parseInt(document.getElementById('altura').value);
-    var base = parseInt(document.getElementById('base').value);
-    var area = base * altura;
-    document.getElementById('resultado_area').textContent = area;
-    console.log(area);
+  var altura = parseInt(document.getElementById("altura").value);
+  var base = parseInt(document.getElementById("base").value);
+  var area = base * altura;
+  document.getElementById("resultado_area").textContent = area;
+  console.log(area);
 }
 
 function calcular_triangulo() {
-    var altura1 = parseInt(document.getElementById('altura1').value);
-    var base1 = parseInt(document.getElementById('base1').value);
-    var area1 = base1 * altura1 / 2;
-    document.getElementById('resultado_area1').textContent = area1;
-    console.log(area1);
+  var altura1 = parseInt(document.getElementById("altura1").value);
+  var base1 = parseInt(document.getElementById("base1").value);
+  var area1 = (base1 * altura1) / 2;
+  document.getElementById("resultado_area1").textContent = area1;
+  console.log(area1);
 }
 
-function Semillero_name(){
-    const nombres = ["Enrique","Jesus","Gustavo", "Johann","Hugo","Luis","Angel","Ali"]
-    document.getElementById('Semillero_name').addEventListener('submit', function(event){
-        event.preventDefault();
-        var Nombre = document.getElementById("N_integrante").value;
-        if(nombres.includes(Nombre)){
-            alert("Eres parte del semillero")
-            
-        }else{
-            alert("No es parte del semillero")
-        }
-
-    } )
-
-    
+function Semillero_name() {
+  const nombres = [
+    "Enrique",
+    "Jesus",
+    "Gustavo",
+    "Johann",
+    "Hugo",
+    "Luis",
+    "Angel",
+    "Ali",
+  ];
+  document
+    .getElementById("Semillero_name")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      var Nombre = document.getElementById("N_integrante").value;
+      if (nombres.includes(Nombre)) {
+        alert("Eres parte del semillero");
+      } else {
+        alert("No es parte del semillero");
+      }
+    });
 }
-Semillero_name()
+Semillero_name();
 
-document.getElementById('generadorContrasenas').addEventListener('submit', function(event){
+document
+  .getElementById("generadorContrasenas")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
     generadorContrasenas();
-})
+  });
 
-function generadorContrasenas(){
-    let contrasena = '';
-    let caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let longitud = parseInt(document.getElementById('ncaracteres').value);
-    for(let i = 0; i < longitud; i++){
-        contrasena += caracteres.charAt(Math.floor(Math.random() * caracteres.length))   
-    }
-    document.getElementById("contrasena").innerHTML = contrasena;
-
-} 
+function generadorContrasenas() {
+  let contrasena = "";
+  let caracteres =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let longitud = parseInt(document.getElementById("ncaracteres").value);
+  for (let i = 0; i < longitud; i++) {
+    contrasena += caracteres.charAt(
+      Math.floor(Math.random() * caracteres.length)
+    );
+  }
+  document.getElementById("contrasena").innerHTML = contrasena;
+}
--- JS/weather.js
+++ JS/weather.js
@@ -1,34 +1,34 @@
-const apikey = '8d01f55a543040d894b174007240506';
+const apikey = "8d01f55a543040d894b174007240506";
 
-function getWeather(){
-    var city = document.getElementById('city').value;
-    fetch(`https://api.weatherapi.com/v1/current.json?key=${apikey}&q=${city}`)
-    .then(response =>{
-        if(!response.ok){
-        throw new Error('esta ciudad no existe como tlaxcala');
-    }
-    return response.json();
-})
-.then(data => {
-    if(data && data.current && data.location){
-        document.getElementById('weather').innerHTML = 
-        `<h2>${data.location.name}, ${data.location.country} <img src = "${data.current.condition.icon}"></img> </h2>` +
-        `<p> Estado: ${data.location.region}</p>` +
-        `<p>Fecha y Hora: ${data.current.last_updated}</p>` +
-        `<p>La temperatura es: ${data.current.temp_c}°C</p>` +
-        `<p>Sensacion real de: ${data.current.feelslike_c}°C</p>`+
-        `<p>La velocidad del viento: ${data.current.wind_kph} km</p>` +
-        `<p>La humedad es: ${data.current.humidity}%</p>` +
-        `<p>Probabilidad de lluvia: ${data.current.precip_in}%</p>` +
-        `<p>La condicion del dia es: ${data.current.condition.text}</p>` +
-        `<p> Nivel de rayos UV: ${data.current.uv}</p>` +
-        `<p>Si tu casa esta a menos de ${data.current.vis_km}km si puedes ver tu casa </p>`
+function getWeather() {
+  var city = document.getElementById("city").value;
+  fetch(`https://api.weatherapi.com/v1/current.json?key=${apikey}&q=${city}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("esta ciudad no existe como tlaxcala");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (data && data.current && data.location) {
+        document.getElementById("weather").innerHTML =
+          `<h2>${data.location.name}, ${data.location.country} <img src = "${data.current.condition.icon}"></img> </h2>` +
+          `<p> Estado: ${data.location.region}</p>` +
+          `<p>Fecha y Hora: ${data.current.last_updated}</p>` +
+          `<p>La temperatura es: ${data.current.temp_c}°C</p>` +
+          `<p>Sensacion real de: ${data.current.feelslike_c}°C</p>` +
+          `<p>La velocidad del viento: ${data.current.wind_kph} km</p>` +
+          `<p>La humedad es: ${data.current.humidity}%</p>` +
+          `<p>Probabilidad de lluvia: ${data.current.precip_in}%</p>` +
+          `<p>La condicion del dia es: ${data.current.condition.text}</p>` +
+          `<p> Nivel de rayos UV: ${data.current.uv}</p>` +
+          `<p>Si tu casa esta a menos de ${data.current.vis_km}km si puedes ver tu casa </p>`;
         /* `<img src = "${data.current.condition.icon}"></img>`  */
-        console.log(data)
-    }
-})
-.catch(error => {
-    alert(error);
-    console.error(error);
-})
+        console.log(data);
+      }
+    })
+    .catch((error) => {
+      alert(error);
+      console.error(error);
+    });
 }
